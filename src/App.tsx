f29a6827438929c446de8d863f63d694b7111912
Stodulski <maxisto043@gmail.com>
2025-06-24 00:24:02 -0300

    new features

--- src/App.tsx
+++ src/App.tsx
@@ -18,11 +18,13 @@ function App () {
           style: {
             padding: '1.5rem 2rem',
             fontSize: '16px',
-            marginTop: '10%', 
+            marginTop: '10%',
             width: '90vw',
             maxWidth: '672px',
             boxShadow: '0 8px 32px rgba(0,0,0,0.1)',
-            borderRadius: '12px'
+            borderRadius: '12px',
+            marginLeft: 'auto',
+            marginRight: 'auto'
           }
         }}
       />

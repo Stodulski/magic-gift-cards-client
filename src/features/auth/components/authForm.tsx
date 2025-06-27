81b6dc7ecfd556f1f3cf7b49a8a5b30484214fc2
Stodulski <maxisto043@gmail.com>
2025-06-27 18:19:50 -0300

    Arreglos de compatibilidad

--- src/features/auth/components/authForm.tsx
+++ src/features/auth/components/authForm.tsx
@@ -22,21 +22,21 @@ export const AuthForm = () => {
       <div className='auth_bg rounded-2xl  transition-all duration-200 flex items-center justify-center w-[90%] max-w-sm flex-col p-8'>
         <img src={magicLogo} className='w-32' alt='Magic Store logo' />
         <div className='mx-auto flex w-full items-center gap-y-3 mt-8 font-semibold text-gray-500 flex-col'>
-          <Input
+          <input
             placeholder='Usuario'
             type='text'
             name='username'
             onChange={handleUsername}
             value={username}
-            className='placeholder:text-black/70 text-black bg-white h-10'
+            className='placeholder:text-black/70 w-full input_auth text-black bg-white h-10'
           />
-          <Input
+          <input
             placeholder='Contraseña'
             type='password'
             name='password'
             onChange={handlePassword}
             value={password}
-            className='placeholder:text-black/70 text-black bg-white h-10'
+            className='placeholder:text-black/70 w-full input_auth text-black bg-white h-10'
           />
           <button className='w-full h-12 cursor-pointer button_bg'>Ingresar</button>
         </div>
